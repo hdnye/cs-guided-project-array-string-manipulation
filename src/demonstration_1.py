b13192a12770cd18d40a196703f70a8695ae3130
Heather Nye <heatherdnye@gmail.com>
2020-12-16 14:20:05 -0600

    demo1

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -20,6 +20,69 @@ Output: -1
 Explanation:
 There is no index that satisfies the conditions in the problem statement.
 """
-def pivot_index(nums):
-    # Your code here
+# def pivot_index(nums):
+#     # Your code here
+#     # iterate through array
+#     for i in range(len(nums)):
+#         # check if current index is the pivot        
+#         left_sub = nums[:i]        
+#         right_sub = nums[i+1:]
+#         # get sum of both subarrays & compare
+#         left_sum = sum(left_sub)
+#         right_sum = sum(right_sub)        
+#         # if equal return i
+#         if left_sum == right_sum:
+#             return i
+# # ^^ O(n^2)
+# # more optimal solution O(n)
 
+# def pivot_index(nums):
+#     l_sum = 0
+#     r_sum = sum(nums[1:])
+
+#     # iterate array & check if i is pivot
+#     for i in range(len(nums)):
+#         if l_sum == r_sum:
+#             return i
+#         # add # at cur index to l_sum 
+#         l_sum += nums[i]
+#         # check we're not at last index
+#         if (i+1 == len(nums)):
+#             r_sum = 0
+#         # remove left sum from right sum. this avoids running the sum loop from above repeatedly
+#         else:
+#             r_sum -= nums[i+1]
+
+#     return -1
+
+# Optimal solution
+def pivot_index(nums):
+    total_sum = sum(nums)
+    l_sum = 0
+
+    for i in range(len(nums)):
+        r_sum = total_sum - l_sum - nums[i]
+        if l_sum == r_sum:
+            return i
+        l_sum += nums[i]
+    return -1
+
+
+
+print(pivot_index([1,7,3,6,5,6]))
+print(pivot_index([1,7,3]))
+    
+'''
+    input = array of ints
+    output = return a number (an index)
+    Plan: search through arary find the pivot index
+    is I a pivot index/ 
+    get sum of left subarray
+    get sum of right subarray
+    compare
+    pseudo:
+    if true
+        - return index(i)
+    if false
+        - keep looping
+'''
